f12cb724116f388cbffe020d118a148c51a1f63b
ikaros <zch13773107361@163.com>
2026-02-22 23:41:41 +0800

    init

---
+++ src/main/java/com/example/communityexpress/CommunityExpressApplication.java
@@ -0,0 +1,13 @@
+package com.example.communityexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityExpressApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityExpressApplication.class, args);
+    }
+
+}
