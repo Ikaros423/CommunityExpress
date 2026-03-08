38af133e66ae4b15284671b51921ac57eed21e37
ikaros <zch13773107361@163.com>
2026-03-08 23:47:49 +0800

    refactor: 调整启动类包路径

--- src/main/java/com/express/system/CommunityExpressApplication.java
+++ src/main/java/com/express/system/CommunityExpressApplication.java
@@ -1,9 +1,11 @@
-package com.example.communityexpress;
+package com.express.system;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.express.system.mapper")
 public class CommunityExpressApplication {
 
     public static void main(String[] args) {
